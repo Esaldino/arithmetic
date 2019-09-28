4933c3a6b3a7478ed8defd4d3718e74a08e85979
pro-it <developer@proit-consulting.com>
2019-09-28 09:23:01 +0100

    first commit

---
+++ src/main/java/com/github/Esaldino/Arithemetic.java
@@ -0,0 +1,10 @@
+package com.github.Esaldino;
+
+import java.math.BigDecimal;
+
+public class Arithemetic {
+    public  BigDecimal add(BigDecimal x, BigDecimal y){return x.add(y);}
+    public  BigDecimal sub(BigDecimal x, BigDecimal y){return x.subtract(y);}
+    public  BigDecimal mul(BigDecimal x, BigDecimal y){return x.multiply(y);}
+    public  BigDecimal div(BigDecimal x, BigDecimal y){return x.divide(y);}
+}
